7c71f856c9b5afaaad9b5dec4b5e393f8f33f0b2
TuanWoox <z1venttt@gmail.com>
2024-03-28 15:32:57 +0700

    Test

--- WinFormCode/FProfile.cs
+++ WinFormCode/FProfile.cs
@@ -39,8 +39,8 @@ namespace WinFormProject
                 rdoMale.Checked = true;
             }
             else rdoFemale.Checked = true;
-            ImageHandler.DisplayImage(jobseeker.Avatar, ref ptbAvatar);
-            ImageHandler.DisplayPdfPreview(jobseeker.CV, ptbCV);
+            if(jobseeker.Avatar!=null) ImageHandler.DisplayImage(jobseeker.Avatar, ref ptbAvatar);
+            if(jobseeker.CV!=null) ImageHandler.DisplayPdfPreview(jobseeker.CV, ptbCV);
         }
         private void btnEdit_Click(object sender, EventArgs e)
         {
@@ -89,7 +89,7 @@ namespace WinFormProject
         private void btnChooseNewCV_Click(object sender, EventArgs e)
         {
             Cv = PDFHandler.OpenFileDialog();
-            ImageHandler.DisplayPdfPreview(Cv, ptbCV);
+            if(Cv!=null) ImageHandler.DisplayPdfPreview(Cv, ptbCV);
 
         }
 
