e43183fb02f12148a13ec4d8dbce1caeb1ec910c
ttoann <ntt26.5.2004@gmail.com>
2024-04-03 15:29:59 +0700

    Update UI: FHistory, UCHistory

---
+++ WinFormCode/FHistory.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WinFormProject.WinFormCode
+{
+    partial class FHistory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblListJobs = new System.Windows.Forms.Label();
+            this.pnSubBody = new System.Windows.Forms.FlowLayoutPanel();
+            this.dungeonComboBox1 = new ReaLTaiizor.Controls.DungeonComboBox();
+            this.SuspendLayout();
+            // 
+            // lblListJobs
+            // 
+            this.lblListJobs.AutoSize = true;
+            this.lblListJobs.BackColor = System.Drawing.Color.White;
+            this.lblListJobs.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblListJobs.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblListJobs.Location = new System.Drawing.Point(26, 27);
+            this.lblListJobs.Name = "lblListJobs";
+            this.lblListJobs.Size = new System.Drawing.Size(126, 46);
+            this.lblListJobs.TabIndex = 6;
+            this.lblListJobs.Text = "History";
+            // 
+            // pnSubBody
+            // 
+            this.pnSubBody.Location = new System.Drawing.Point(90, 112);
+            this.pnSubBody.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pnSubBody.Name = "pnSubBody";
+            this.pnSubBody.Size = new System.Drawing.Size(856, 296);
+            this.pnSubBody.TabIndex = 7;
+            // 
+            // dungeonComboBox1
+            // 
+            this.dungeonComboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(246)))));
+            this.dungeonComboBox1.ColorA = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(132)))), ((int)(((byte)(85)))));
+            this.dungeonComboBox1.ColorB = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(108)))), ((int)(((byte)(57)))));
+            this.dungeonComboBox1.ColorC = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(241)))), ((int)(((byte)(240)))));
+            this.dungeonComboBox1.ColorD = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.dungeonComboBox1.ColorE = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(237)))), ((int)(((byte)(236)))));
+            this.dungeonComboBox1.ColorF = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.dungeonComboBox1.ColorG = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(119)))), ((int)(((byte)(118)))));
+            this.dungeonComboBox1.ColorH = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(222)))), ((int)(((byte)(220)))));
+            this.dungeonComboBox1.ColorI = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(249)))), ((int)(((byte)(249)))));
+            this.dungeonComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.dungeonComboBox1.DropDownHeight = 250;
+            this.dungeonComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.dungeonComboBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.dungeonComboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(76)))), ((int)(((byte)(97)))));
+            this.dungeonComboBox1.FormattingEnabled = true;
+            this.dungeonComboBox1.HoverSelectionColor = System.Drawing.Color.Empty;
+            this.dungeonComboBox1.IntegralHeight = false;
+            this.dungeonComboBox1.ItemHeight = 30;
+            this.dungeonComboBox1.Items.AddRange(new object[] {
+            "All",
+            "Waiting",
+            "Done"});
+            this.dungeonComboBox1.Location = new System.Drawing.Point(839, 35);
+            this.dungeonComboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dungeonComboBox1.Name = "dungeonComboBox1";
+            this.dungeonComboBox1.Size = new System.Drawing.Size(176, 36);
+            this.dungeonComboBox1.StartIndex = 0;
+            this.dungeonComboBox1.TabIndex = 2;
+            // 
+            // FHistory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1048, 600);
+            this.Controls.Add(this.dungeonComboBox1);
+            this.Controls.Add(this.pnSubBody);
+            this.Controls.Add(this.lblListJobs);
+            this.Name = "FHistory";
+            this.Text = "FHistory";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblListJobs;
+        private FlowLayoutPanel pnSubBody;
+        private ReaLTaiizor.Controls.DungeonComboBox dungeonComboBox1;
+    }
+}
