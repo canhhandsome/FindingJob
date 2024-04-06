58b6c5f97df8aaf0ae9bccb1d65c60defd5b6132
canhhandsome <cbd1708@gmail.com>
2024-04-06 17:59:37 +0700

    test

--- obj/Debug/net6.0-windows/WinFormProject.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/WinFormProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b321776625a108f5fb5c0034aab0c8dc4844248")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bb5b829aabe49f46f203600259a44473386ef46")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
